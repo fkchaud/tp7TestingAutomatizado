d6fa35c14143abfb973a3d34daecc77ecfccfed1
Gonzalo <ggbertomeu@gmail.com>
2017-10-24 21:14:17 -0300

    Creado proyecto Java en NetBeans para usar Selenium Webdriver

---
+++ TP7 Selenium Webdriver/src/tp7/selenium/webdriver/TP7SeleniumWebdriver.java
@@ -0,0 +1,12 @@
+package tp7.selenium.webdriver;
+
+public class TP7SeleniumWebdriver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
