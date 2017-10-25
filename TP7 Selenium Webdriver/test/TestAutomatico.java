c0b2a6b2a68875484dd0fa766cdac8f4c9748bb2
pablobaldini94 <pablobaldini94@hotmail.com>
2017-10-24 22:48:47 -0300

    Encerrado en Try Catch para captar excepciones como fallos

--- TP7 Selenium Webdriver/test/TestAutomatico.java
+++ TP7 Selenium Webdriver/test/TestAutomatico.java
@@ -15,81 +15,86 @@ public class TestAutomatico {
 
     @Test
     public void pruebaTP() {
-        //inicializar Gecko Driver
-        System.setProperty("webdriver.gecko.driver", "lib\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+        try {
+            //inicializar Gecko Driver
+            System.setProperty("webdriver.gecko.driver", "lib\\geckodriver-v0.19.0-win64\\geckodriver.exe");
 
-        //Inicializar driver
-        WebDriver driver = new FirefoxDriver();
+            //Inicializar driver
+            WebDriver driver = new FirefoxDriver();
 
-        //Abrir URL
-        driver.get("http://newtours.demoaut.com/");
-        
-        //clickear REGISTER
-        WebElement register = driver.findElement(By.linkText("REGISTER"));
-        register.click();
-        
-        //ingresar usuario
-        WebElement email = driver.findElement(By.id("email"));
-        email.sendKeys("admin");
+            //Abrir URL
+            driver.get("http://newtours.demoaut.com/");
 
-        //ingresar password
-        WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys("admin");
-        WebElement confirmPassword = driver.findElement(By.name("confirmPassword"));
-        confirmPassword.sendKeys("admin");
-        
-        //solucionar problema "Contraseña Insegura" de Firefox
-        email.click();
+            //clickear REGISTER
+            WebElement register = driver.findElement(By.linkText("REGISTER"));
+            register.click();
 
-        //clickear Register
-        WebElement register2 = driver.findElement(By.name("register"));
-        register2.click();
-        
-        //assert que esté el "SIGN-OFF"
-        try{
-            WebElement signoff = driver.findElement(By.linkText("SIGN-OFF"));
-        } catch (NoSuchElementException e) {
+            //ingresar usuario
+            WebElement email = driver.findElement(By.id("email"));
+            email.sendKeys("admin");
+
+            //ingresar password
+            WebElement password = driver.findElement(By.name("password"));
+            password.sendKeys("admin");
+            WebElement confirmPassword = driver.findElement(By.name("confirmPassword"));
+            confirmPassword.sendKeys("admin");
+
+            //solucionar problema "Contraseña Insegura" de Firefox
+            email.click();
+
+            //clickear Register
+            WebElement register2 = driver.findElement(By.name("register"));
+            register2.click();
+
+            //assert que esté el "SIGN-OFF"
+            try{
+                WebElement signoff = driver.findElement(By.linkText("SIGN-OFF"));
+            } catch (NoSuchElementException e) {
+                fail(e.getMessage());
+            }
+
+            //clickear Flights
+            WebElement flights = driver.findElement(By.linkText("Flights"));
+            flights.click();
+
+            //seleccionar name=fromPort -> label=London
+            Select fromPort = new Select(driver.findElement(By.name("fromPort")));
+            fromPort.selectByVisibleText("London");
+
+            //seleccionar name=toPort -> label=Paris
+            Select toPort = new Select(driver.findElement(By.name("toPort")));
+            toPort.selectByVisibleText("Paris");
+
+            //seleccionar primera clase name=servClass value=First
+            List<WebElement> servClass = driver.findElements(By.name("servClass"));
+            servClass.get(2).click();
+
+            //seleccionar name=findFlights
+            WebElement findFlights = driver.findElement(By.name("findFlights"));
+            findFlights.click();
+
+            //seleccionar name=reserveFlights
+            WebElement reserveFlights = driver.findElement(By.name("reserveFlights"));
+            reserveFlights.click();
+
+            //ingresar nombre, apellido, y numero de tarjeta
+            WebElement passFirst0 = driver.findElement(By.name("passFirst0"));
+            passFirst0.sendKeys("Bruno");
+            WebElement passLast0 = driver.findElement(By.name("passLast0"));
+            passLast0.sendKeys("Diaz");
+            WebElement creditnumber = driver.findElement(By.name("creditnumber"));
+            creditnumber.sendKeys("123456789012");
+
+            //seleccionar name=buyFlights
+            WebElement buyFlights = driver.findElement(By.name("buyFlights"));
+            buyFlights.click();
+
+            //seleccionar LOG-OUT
+            List<WebElement> img = driver.findElements(By.tagName("img"));
+            img.get(10).click();
+        }
+        catch (Exception e) {
             fail(e.getMessage());
         }
-        
-        //clickear Flights
-        WebElement flights = driver.findElement(By.linkText("Flights"));
-        flights.click();
-        
-        //seleccionar name=fromPort -> label=London
-        Select fromPort = new Select(driver.findElement(By.name("fromPort")));
-        fromPort.selectByVisibleText("London");
-
-        //seleccionar name=toPort -> label=Paris
-        Select toPort = new Select(driver.findElement(By.name("toPort")));
-        toPort.selectByVisibleText("Paris");
-        
-        //seleccionar primera clase name=servClass value=First
-        List<WebElement> servClass = driver.findElements(By.name("servClass"));
-        servClass.get(2).click();
-
-        //seleccionar name=findFlights
-        WebElement findFlights = driver.findElement(By.name("findFlights"));
-        findFlights.click();
-
-        //seleccionar name=reserveFlights
-        WebElement reserveFlights = driver.findElement(By.name("reserveFlights"));
-        reserveFlights.click();
-        
-        //ingresar nombre, apellido, y numero de tarjeta
-        WebElement passFirst0 = driver.findElement(By.name("passFirst0"));
-        passFirst0.sendKeys("Bruno");
-        WebElement passLast0 = driver.findElement(By.name("passLast0"));
-        passLast0.sendKeys("Diaz");
-        WebElement creditnumber = driver.findElement(By.name("creditnumber"));
-        creditnumber.sendKeys("123456789012");
-
-        //seleccionar name=buyFlights
-        WebElement buyFlights = driver.findElement(By.name("buyFlights"));
-        buyFlights.click();
-        
-        //seleccionar LOG-OUT
-        List<WebElement> img = driver.findElements(By.tagName("img"));
-        img.get(10).click();
     }
 }
